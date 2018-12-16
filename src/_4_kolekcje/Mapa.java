0bc41a8f3069bd9d0c71a6d75554cd0f52fc90f8
Justyna <blachutjvstyna@gmail.com>
2018-12-16 16:49:51 +0100

    Commit poprzez IntelliJ

--- src/_4_kolekcje/Mapa.java
+++ src/_4_kolekcje/Mapa.java
@@ -1,4 +1,19 @@
 package _4_kolekcje;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Mapa {
+    public static void main(String[] args) {
+        Map<String,Integer> inwentarz = new HashMap<>();
+        inwentarz.put("Mysz",4);
+        inwentarz.put("Monitor",2);
+        inwentarz.put("Glosniki", 10);
+
+        System.out.println(inwentarz);
+        inwentarz.put("Monitor",21);
+        System.out.println(inwentarz); // dane się nadpisują w mapie, klucze muszą być unikalne
+
+
+    }
 }
