c96391257b7129ad932baa576734ae3e1c0a96d1
Justyna <blachutjvstyna@gmail.com>
2018-12-16 16:58:28 +0100

    Commit poprzez IntelliJ

--- src/_4_kolekcje/Mapa.java
+++ src/_4_kolekcje/Mapa.java
@@ -2,10 +2,11 @@ package _4_kolekcje;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Mapa {
     public static void main(String[] args) {
-        Map<String,Integer> inwentarz = new HashMap<>();
+        Map<String,Integer> inwentarz = new TreeMap<>();
         inwentarz.put("Mysz",4);
         inwentarz.put("Monitor",2);
         inwentarz.put("Glosniki", 10);
@@ -14,6 +15,17 @@ public class Mapa {
         inwentarz.put("Monitor",21);
         System.out.println(inwentarz); // dane się nadpisują w mapie, klucze muszą być unikalne
 
+        System.out.println("Stan Głośników: " + inwentarz.get("Glosniki"));
 
+        System.out.println(inwentarz.values());
+        System.out.println(inwentarz.keySet());
+        System.out.println(inwentarz.containsKey("Glosniki"));
+        System.out.println(inwentarz.containsValue(10));
+        int suma = 0;
+
+        for (String klucz : inwentarz.keySet()) {
+            suma=suma+inwentarz.get(klucz);
+        }
+        System.out.println("suma produktów: " + suma);
     }
 }
