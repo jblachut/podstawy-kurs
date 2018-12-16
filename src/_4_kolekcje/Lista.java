28b6462b6ea4395eb107cb672cef759a98cf8b00
Justyna <blachutjvstyna@gmail.com>
2018-12-16 15:44:41 +0100

    Commit poprzez IntelliJ

---
+++ src/_4_kolekcje/Lista.java
@@ -0,0 +1,19 @@
+package _4_kolekcje;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lista {
+    public static void main(String[] args) {
+        //List imiona; // zadeklarowanie zmiennej listy imion
+       // List imiona = new ArrayList<>(); //Arraylist - lista oparta na tablicy
+        List<String> imiona = new ArrayList<>();
+        imiona.add("Wojciech");
+        imiona.add("Marcin");
+        imiona.add("Sylwia");
+
+        System.out.println(imiona);
+        System.out.println("Rozmiar: " + imiona.size());
+        System.out.println("Pozycja 1: " + imiona.get(1));
+    }
+}
