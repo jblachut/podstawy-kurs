968b9d69d4d89dce1773800120974b274cd3b1e0
Justyna <blachutjvstyna@gmail.com>
2018-12-16 15:54:50 +0100

    Commit poprzez IntelliJ

--- src/_4_kolekcje/Lista.java
+++ src/_4_kolekcje/Lista.java
@@ -10,11 +10,20 @@ public class Lista {
         List<String> imiona = new ArrayList<>();
         imiona.add("Wojciech");
         imiona.add("Justyna");
-        imiona.add("Sylwia");
+        imiona.add("Marcin");
         imiona.add("Sylwia");
 
         System.out.println(imiona);
         System.out.println("Rozmiar: " + imiona.size());
         System.out.println("Pozycja 1: " + imiona.get(1));
+
+        for (String imie : imiona){
+            if (imie.endsWith("a")) {
+                System.out.println(imie);
+                break; //zatrzymuje po 1 pętli
+            }
+        }
+        imiona.remove("Wojciech");
+        System.out.println(imiona);
     }
 }
