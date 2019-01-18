b765d88be0801274051fdc433bfea7491b492f27
jblachut <blachutjvstyna@gmail.com>
2019-01-18 17:58:16 +0100

    n

---
+++ src/_5_zadanie/Produkt.java
@@ -0,0 +1,18 @@
+package _5_zadanie;
+
+import java.math.BigDecimal;
+
+public class Produkt {
+    String nazwa;
+    BigDecimal cena;
+    int ilosc;
+
+    public Produkt(String nazwa, BigDecimal cena, int ilosc) {
+        this.nazwa=nazwa;
+        this.cena=cena;
+        this.ilosc=ilosc;
+
+
+    }
+
+}
---
+++ src/_5_zadanie/Zakupy.java
@@ -0,0 +1,42 @@
+package _5_zadanie;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+public class Zakupy {
+
+    public static void main(String[] args) {
+        Scanner wejscie = new Scanner(System.in);
+        ArrayList<Produkt> produkt1 = new ArrayList<>();
+        char end = 'T';
+        String nazwa;
+        BigDecimal cena;
+        int ilosc;
+
+        while (end == 'T') {
+            System.out.print("Podaj nazwę produktu: ");
+            nazwa = wejscie.nextLine();
+            System.out.print("Podaj cenę produktu: ");
+            cena = wejscie.nextBigDecimal();
+            System.out.print("Podaj ilość sztuk: ");
+            ilosc = wejscie.nextInt();
+
+            produkt1.add(new Produkt(nazwa, cena, ilosc));
+
+            System.out.println(" Wprowadzic kolejną pozycję na liście? (T/N)");
+            String word = wejscie.next();
+            word = word.toUpperCase();
+            end = word.charAt(0);
+
+            System.out.println("Lista zawiera : "+produkt1.size() + "produkty");
+            System.out.println("Lista zawiera : "+produkt1.size() + "produkty");
+
+        }
+
+
+
+
+    }
+}
