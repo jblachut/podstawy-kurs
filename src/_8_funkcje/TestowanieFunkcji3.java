5bb1c4d9769b74feb7dd09a2939b0f5ef3e13ddd
Justyna <blachutjvstyna@gmail.com>
2019-01-19 15:36:35 +0100

    Commit poprzez IntelliJ

--- src/_8_funkcje/TestowanieFunkcji3.java
+++ src/_8_funkcje/TestowanieFunkcji3.java
@@ -36,17 +36,17 @@ public class TestowanieFunkcji3 {
             System.out.println("2 mile = " + mltokm(2) + " km" );
             System.out.println();
 
-            System.out.println("160 km = " + KonwersjaJednostek.kmtoml(160.0) + " mil" );
-            System.out.println("10 km = " + KonwersjaJednostek.kmtoml(10.0) + " mil" );
+            System.out.println("160 km = " + kmtoml(160.0) + " mil" );
+            System.out.println("10 km = " + kmtoml(10.0) + " mil" );
             System.out.println();
 
-            System.out.println("0 C = " + KonwersjaJednostek.ctofr(0.0) + " F" );
-            System.out.println("100 C = " + KonwersjaJednostek.ctofr(100.0) + " F" );
-            System.out.println("37 C = " + KonwersjaJednostek.ctofr(37.0) + " F" );
+            System.out.println("0 C = " + ctofr(0.0) + " F" );
+            System.out.println("100 C = " + ctofr(100.0) + " F" );
+            System.out.println("37 C = " + ctofr(37.0) + " F" );
 
-            System.out.println("0 F = " + KonwersjaJednostek.frtoc(0.0) + " C" );
-            System.out.println("100 F = " + KonwersjaJednostek.frtoc(100.0) + " C" );
-            System.out.println("32 F = " + KonwersjaJednostek.frtoc(32.0) + " C" );
+            System.out.println("0 F = " + frtoc(0.0) + " C" );
+            System.out.println("100 F = " + frtoc(100.0) + " C" );
+            System.out.println("32 F = " + frtoc(32.0) + " C" );
 
         }
 }
