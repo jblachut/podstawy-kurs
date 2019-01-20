ce1ef7872317e78254e15ef3c4f904e278fe0081
Justyna <blachutjvstyna@gmail.com>
2019-01-20 11:07:51 +0100

    Commit poprzez IntelliJ

---
+++ src/_9_kalkulator/KlakulatorKonsolowy.java
@@ -0,0 +1,43 @@
+package _9_kalkulator;
+
+import java.util.Scanner;
+
+public class KlakulatorKonsolowy {
+    public static void main(String[] args) {
+        System.out.println("Podaj kolejne działania w formie x + x");
+        System.out.println("Zakończ podając literę");
+
+        Scanner wejscie = new Scanner(System.in);
+        while (true) { //rozpoczyna pętle bo pyta cały czas
+            System.out.println(">");
+            try { //łapanie wyjątku
+                double arg1 = wejscie.nextDouble();  //delitherem jest spacja next wpisze 1 token
+                String operacja = wejscie.next();    // nextline - całość, next do spacji
+                double arg2 = wejscie.nextDouble();
+                double wynik;
+
+                if ("+".equals(operacja)) {
+                    wynik = arg1 + arg2;
+                    System.out.println(wynik);
+                } else if ("-".equals(operacja)) { //porównywanie stringów !!
+                    wynik = arg1 - arg2;
+                    System.out.println(wynik);
+                } else if ("*".equals(operacja)) {
+                    wynik = arg1 * arg2;
+                    System.out.println(wynik);
+                } else if ("/".equals(operacja)) {
+                    wynik = arg1 / arg2;
+                    System.out.println(wynik);
+
+
+                }
+
+
+            } catch (Exception e) {
+                System.out.println("Do widzenia");
+                break;
+
+            }
+        }
+    }
+}
