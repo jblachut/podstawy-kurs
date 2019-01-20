da2088f191d62f200f7de9497a4b3f377670e413
Justyna <blachutjvstyna@gmail.com>
2019-01-20 10:15:53 +0100

    Commit poprzez IntelliJ

--- src/_7_petle/SumaParzystych.java
+++ src/_7_petle/SumaParzystych.java
@@ -1,5 +1,6 @@
 package _7_petle;
 
+import java.util.OptionalInt;
 import java.util.stream.IntStream;
 
 public class SumaParzystych {
@@ -14,18 +15,64 @@ public class SumaParzystych {
             }
         }
 
-            return wynik;
-        }
+        return wynik;
+    }
+
     static int suma_stream(int[] tablica) {
-        return IntStream.of(tablica).filter(x -> x%2==0).sum();
+        return IntStream.of(tablica).filter(x -> x % 2 == 0).sum();
+    }
+
+    // w przypadku gydy wniku może nie być tworzymy clase intiger zamiast typu int
+    static Integer max(int[] tablica) { //integer bo musimy zwrócić nulla
+        if (tablica.length == 0) {
+            return null;
+        }
+        int max = tablica[0];
+        //for (int x = 0; x < tablica.length; x++) { //definicja pętli pozycja
+        // liczby w tablicy - i =0 i jest mniejsze niż wielkość tablicy, i dodaj do i 1
+        //przy int x=0 itd trzeba wpsywać tablica[x], w nowszej wersji nie trzeba - wystarczy x
+        for (int x : tablica)
+            if (x > max) {
+                max = x;
+            }
+        return max;
+    }
+
+    //nowoczesne podejście do wartości opcjonalncy - soecialna klasa opcional
+    static OptionalInt maxx(int[] tablica) { //integer bo musimy zwrócić nulla
+
+        if (tablica.length == 0) {
+            return OptionalInt.empty();
+        }
+        int maxx = tablica[0];
+        //for (int x = 0; x < tablica.length; x++) { //definicja pętli pozycja
+        // liczby w tablicy - i =0 i jest mniejsze niż wielkość tablicy, i dodaj do i 1
+        //przy int x=0 itd trzeba wpsywać tablica[x], w nowszej wersji nie trzeba - wystarczy x
+        for (int x : tablica)
+            if (x > maxx) {
+                maxx = x;
+            }
+        return OptionalInt.of(maxx);
     }
 
     //dla wygody maina testującego umieszczamy w tym samym pliku
     public static void main (String[]args){
         int[] a = {10, 20, 30, 5, 10, 15};
-        int[] b = {-20, 0, 5, 20,};
+        int[] b = {-20, -10, -5, -20};
+        int[] c = {};
 
-        System.out.println("suma a : " + suma_stream(a));
-        System.out.println("suma b : " + suma(b));
+        System.out.println("max a : " + max(a));
+        System.out.println("max b : " + max(b));
+        System.out.println("max c : " + max(c));
+
+        OptionalInt opt = maxx(c);
+        if (opt.isPresent()){
+            System.out.println("Wynik wynosi " +opt.getAsInt());
+        } else {
+            System.out.println("Brak wyniku");
+        }
     }
 }
+
+
+//kiedy nie ma wyniku
