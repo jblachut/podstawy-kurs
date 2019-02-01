b02681342468f97639d6de9a77f69efc5bac3c91
jblachut <blachutjvstyna@gmail.com>
2019-02-01 21:10:40 +0100

    n

--- src/_12_zadanie_petle_tablice/PorownywanieTablic2.java
+++ src/_12_zadanie_petle_tablice/PorownywanieTablic2.java
@@ -1,9 +1,11 @@
 package _12_zadanie_petle_tablice;
 
+import java.util.ArrayList;
+import java.util.List;
 
-
-public class PorownanieTablic {
+public class PorownywanieTablic2 {
     public static void main(String[] args) {
+        List<Integer> wspolne = new ArrayList<>();
 
         int[] a = { 1, 2, 3, 4, 5, 6, 7, 9, 55 };
         int[] b = { 1, 3, 5, 6, 7, 15, 9, 55, 0 };
@@ -12,17 +14,20 @@ public class PorownanieTablic {
         for (int element1 : a) {
             for (int i : b) {
                 if (i == element1) {
-                    System.out.println(i);
+                    wspolne.add(element1);
                 }
             }
 
 
 
         }
+        System.out.println(wspolne);
 
-
-
-        }
 
     }
+
+}
+
+
+
 
